75d13646b304cb4ac52f46f59bc572bfc9493c05
Jochen Weber <Jochen83@gmx.de>
2025-11-01 21:10:55 +0100

    funktionierende text datei

--- app1.py
+++
@@ -1,3 +0,0 @@
-print("Hello Wolli")
-print("Hello Wolli again")
-print("Hello Wolli again and again")
--- app2.py
+++
@@ -1,4 +0,0 @@
-
-#'Git_Test630Nov
-print("jetzt ist der kleine Haase gefragt!")
-print("jetzt ist der kleine Haase schon wieder gefragt!")
